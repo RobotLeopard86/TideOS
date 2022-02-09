dbea4e6def449276d0034d5badf2d773d63e9b8d
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2022-02-09 17:46:02 -0600

    Added welcome text to sign-on screen

--- Code/OS/.tide_os/programs/boot/signon.lua
+++ Code/OS/.tide_os/programs/boot/signon.lua
@@ -2,6 +2,13 @@ local bg = paintutils.loadImage("/.tide_os/assets/images/signon")
 term.clear()
 paintutils.drawImage(bg, 1, 1)
 
+local w, h = term.getSize()
+
+term.setTextColor(colors.blue)
+term.setCursorPos((w / 3) - 3.5, (h / 4))
+
+print("Welcome")
+
 while true do
     sleep(0.05)
 end
