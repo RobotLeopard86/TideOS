2a58792df15577c139fff6c6bc2633042b689c1d
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2022-06-20 20:39:41 -0500

    Added process killing and listing

--- Code/OS/.tide_os/programs/desktopmanager.lua
+++ Code/OS/.tide_os/programs/desktopmanager.lua
@@ -2,11 +2,24 @@ term.setTextColor(colors.lime)
 term.setBackgroundColor(colors.green)
 term.clear()
 term.setCursorPos(1, 1)
+
+sleep(0.1)
+
+multishell.kill(multishell.getByTitle("signon.lua"))
+
 print("Hi, " .. user_data.username .. "!")
 print() 
 print("Welcome to your boring, generic desktop!")
 print() 
 print("You have " .. user_data.permissions .. " permission! Enjoy!")
+print()
+print("There are " .. multishell.getCount() .. " tabs open:")
+
+local tabs = multishell.getTitles()
+
+for _, tab in ipairs(tabs) do
+    print(tab)
+end
 
 while true do
     sleep(0.05)
