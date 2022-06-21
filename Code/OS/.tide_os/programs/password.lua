afbdb769da9794a5c2321b4ece05923066f8d94e
RobotLeopard86 <63123751+RobotLeopard86@users.noreply.github.com>
2022-06-20 19:27:11 -0500

    Removed no password handling
    This feature is being removed due to technical difficulties

--- Code/OS/.tide_os/programs/password.lua
+++ Code/OS/.tide_os/programs/password.lua
@@ -12,17 +12,10 @@ local perm = usrf.readLine()
 
 usrf.close()
 
-local function youAreIn()
-    term.setBackgroundColor(colors.lightBlue)
-    term.setCursorPos((w / 2) - 14, (h / 6) + 1)
-    term.setTextColor(colors.green)
-    print("          Welcome!          ")
-    term.setCursorPos((w / 2) - 5, (h / 3) * 2)
-    sleep(1)
-
+local function launchDesktop()
     user_data.username = user
     user_data.permissions = perm
-    
+
     local id = multishell.launch({
         ["shell"] = shell,
         ["multishell"] = multishell,
@@ -32,11 +25,17 @@ local function youAreIn()
     multishell.setFocus(id)
 
     shell.exit()
+    return
 end
 
-if pass == "<none>" then
-    youAreIn()
-    return
+local function youAreIn()
+    term.setBackgroundColor(colors.lightBlue)
+    term.setCursorPos((w / 2) - 14, (h / 6) + 1)
+    term.setTextColor(colors.green)
+    print("          Welcome!          ")
+    term.setCursorPos((w / 2) - 5, (h / 3) * 2)
+    sleep(1)
+    launchDesktop()
 end
 
 local bg = paintutils.loadImage("/.tide_os/assets/images/password.ccpaint")
@@ -75,6 +74,7 @@ local function getPassword()
 
     if uinHashed == pass then
         youAreIn()
+        shell.exit()
         return
     else
         term.setBackgroundColor(colors.lightBlue)
